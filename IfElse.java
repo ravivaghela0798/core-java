d72901df2cbb811a12dfdd68f12f53a4f89ebb92
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 15:51:18 +0530

    changes in if else

--- IfElse.java
+++ IfElse.java
@@ -4,7 +4,7 @@ public class IfElse {
         int age = 24;
 
         //Use of if else Condition to varify conditions 
-        if(age <= 18){
+        if(age >= 18){
             System.out.println("You are eligible for Vote");
         } else {
             System.out.println("You are not eligble for Vote");
