2388022d173adfc756b61c4d0c1daed453634750
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 15:48:19 +0530

    if else file commit

---
+++ IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse {
+    public static void main(String[] args) {
+        // Intitialize Integer variable and Assign value to this Variable        
+        int age = 24;
+
+        //Use of if else Condition to varify conditions 
+        if(age <= 18){
+            System.out.println("You are eligible for Vote");
+        } else {
+            System.out.println("You are not eligble for Vote");
+        }
+    }
+}
