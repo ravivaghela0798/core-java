1cdc0d12ed761fa9ca29fbca851e10e0aa47b5f2
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 16:59:36 +0530

    break and continue file commit

---
+++ BreakContinue.java
@@ -0,0 +1,16 @@
+public class BreakContinue {
+    public static void main(String[] args) {
+        for(int i=1; i<=10; i++) {
+            //when i = 5 then skip 5 number 
+            if(i==5) {
+                continue;
+            }
+            System.out.println("Number "+ i);
+            //when i = 7 then break loop 
+            if(i==7)  {
+                break;
+            }
+        }
+    }
+    
+}
