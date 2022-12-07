04772d99cc02b067e7f1a74a84702b4631d1990f
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 16:25:14 +0530

    changes in while loop file

--- WhileLoop.java
+++ WhileLoop.java
@@ -1,7 +1,7 @@
 public class WhileLoop {
     public static void main(String[] args) {
         //Initialize variable for i and assign value to the variable
-        int i = 0;
+        int i = 1;
         //Using while loop iterate value and print number
         while(i<=10) {
             System.out.println("Number " + i);
