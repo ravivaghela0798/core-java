58a0f216775b5d136d2f1723c0dccecd51c24226
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 19:15:52 +0530

    Wrapper Class file commit

---
+++ WrapperClass.java
@@ -0,0 +1,11 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        int a = 15;
+        Integer b = 10;
+        
+        System.out.println(a == b);
+        System.out.println(b.equals(a));
+        
+    }
+    
+}
