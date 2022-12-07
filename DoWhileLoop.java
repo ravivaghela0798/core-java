83d8823c1480c00f3fd38bf8ee7bd3601c16e452
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 16:25:44 +0530

    do while loop file commit

---
+++ DoWhileLoop.java
@@ -0,0 +1,13 @@
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+        //Initialize variable for i and assign value to the variable
+        int i = 1;
+        //Using while loop iterate value and print number
+        do {
+            System.out.println("Number " + i);
+            i++;
+        } while(i<=10);
+    }
+    
+}
