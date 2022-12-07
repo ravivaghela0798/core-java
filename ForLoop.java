706546692eb8f81bab27c871510b9ceaba852e6a
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 16:46:20 +0530

    for loop file commit

---
+++ ForLoop.java
@@ -0,0 +1,13 @@
+public class ForLoop {
+    public static void main(String[] args) {
+        /* define for loop and initialize i value in for loop condition 
+        and iterate the value and print the star */
+        for(int i=0; i<5; i++) {
+            for(int j=0; j<=i; j++){
+                System.out.print("* "); 
+            }
+            System.out.println();
+        }
+    }
+    
+}
