b702569f2f41acf2ec9e46d0a1f07cbdaef7d9e6
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-07 16:19:12 +0530

    while loop file commit

---
+++ WhileLoop.java
@@ -0,0 +1,11 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        //Initialize variable for i and assign value to the variable
+        int i = 0;
+        //Using while loop iterate value and print number
+        while(i<=10) {
+            System.out.println("Number " + i);
+            i++;
+        }
+    }
+}
