2fdd2e3db6632ea6fa75cc229811f7d8313ed059
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-08 15:32:25 +0530

    Encapsulation file commit

---
+++ Encapsulation.java
@@ -0,0 +1,17 @@
+public class Encapsulation {
+    private String name;
+    
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Encapsulation enc = new Encapsulation();
+        enc.setName("Ravi Vaghela");
+        System.out.println(enc.getName());
+    }
+}
