85150dc92cdfb4b20400ac9eb0df9c3a7b23e03e
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-08 13:05:20 +0530

    super this file commit

---
+++ SuperThis.java
@@ -0,0 +1,17 @@
+class Super{
+    int a = 50;
+    int b = 100;
+}
+public class SuperThis extends Super {
+    void res(){
+        int a = 1000;
+        int b = 5000;
+        System.out.println(super.a);
+        System.out.println(a);
+        System.out.println(super.b);
+        System.out.println(b);
+    }
+    public static void main(String[] args) {
+        new SuperThis().res();
+    }
+}
