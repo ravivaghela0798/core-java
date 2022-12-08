5e91513bc809db5595b8eeae402ab1c6e1d0462e
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-08 10:25:19 +0530

    changes in wrapper class file

--- WrapperClass.java
+++ WrapperClass.java
@@ -1,6 +1,6 @@
 public class WrapperClass {
     public static void main(String[] args) {
-        int a = 15;
+        int a = 10;
         Integer b = 10;
         
         System.out.println(a == b);
