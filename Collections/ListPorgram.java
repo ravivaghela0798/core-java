2cd441def5ac0a540e42e2ddaf59f74455ee58c1
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-08 18:48:12 +0530

    collection file commit

---
+++ Collections/ListPorgram.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class ListPorgram {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList();
+        list.add("Ravi Vaghela");
+        list.add("Dipak Paramar");
+        list.add("Juned Davda");
+        System.out.println(list);
+        for (String name : list) {
+            System.out.println(name);
+        }
+    }
+}
---
+++ Collections/SetProgram.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SetProgram {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("Dipak");
+        set.add("Ravi");
+        set.add("Juned");
+        System.out.println(set);
+        for (String name : set) {
+            System.out.println(name);
+        }
+    }
+}
