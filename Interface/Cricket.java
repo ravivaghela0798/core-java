2653a8ddb4820889ec25da54e5f5830250e99662
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-08 16:24:33 +0530

    Interface file commit

---
+++ Interface/Cricket.java
@@ -0,0 +1,15 @@
+public class Cricket implements Sports {
+    @Override
+    public void setHomeTeam(String name){
+        System.out.println("Home Team : " + name);
+    }
+    @Override
+    public void setVisitingTeam(String name){
+        System.out.println("Visiting Team : " + name);
+    }
+    public static void main(String[] args) {
+        Cricket cricket = new Cricket();
+        cricket.setHomeTeam("India");
+        cricket.setVisitingTeam("Australia");
+    }
+}
---
+++ Interface/Sports.java
@@ -0,0 +1,4 @@
+interface Sports {
+    public void setHomeTeam(String name);
+    public void setVisitingTeam(String name);
+}
