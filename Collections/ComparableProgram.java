4f42ed47ae1772b828cd3f91e3d95f8fc8819ae2
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-09 11:20:01 +0530

    changes in comparable

--- Collections/ComparableProgram.java
+++ Collections/ComparableProgram.java
@@ -9,27 +9,35 @@ class Person implements Comparable<Person> {
         this.age = age;
     }
 
-    public int compareTo(Person p) {
-        if (age == p.age) {
-            return 0;
-        } else if (age > p.age) {
-            return 1;
-        } else {
-            return -1;
-        }
+    @Override
+    public int compareTo(Person person) {
+        return this.age - person.age;
+        // if (age == person.age) {
+        //     return 0;
+        // } else if (age > person.age) {
+        //     return 1;
+        // } else {
+        //     return -1;
+        // }
+    }
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
     }
 }
 
 public class ComparableProgram {
     public static void main(String args[]) {
-        ArrayList<Person> al = new ArrayList<Person>();
-        al.add(new Person("Ravi", 24));
-        al.add(new Person("Dipak", 23));
-        al.add(new Person("Juned", 23));
+        ArrayList<Person> list = new ArrayList<>();
+        list.add(new Person("Ravi", 24));
+        list.add(new Person("Dipak", 23));
+        list.add(new Person("Juned", 23));
 
-        Collections.sort(al);
-        for (Person p : al) {
-            System.out.println(p.name + " " + p.age);
+        System.out.println(list);
+        Collections.sort(list);
+        for (Person person : list) {
+            System.out.println(person.name + " " + person.age);
         }
     }
 }
