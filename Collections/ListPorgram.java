76bb28f0efa3ccdc889c9921b042b7e230adec62
ravivaghela07908 <vaghelarv6-00@gmail.com>
2022-12-09 13:02:24 +0530

    changes in list Program

--- Collections/ListPorgram.java
+++ Collections/ListPorgram.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class ListPorgram {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList();
+        ArrayList<String> list = new ArrayList<>();
         list.add("Ravi Vaghela");
         list.add("Dipak Paramar");
         list.add("Juned Davda");
